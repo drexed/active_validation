dd219ca87563683e30ae04f9aa75f10f3a0776ae
Juan Gomez <j.gomez@drexed.com>
2016-10-19 18:53:04 -0400

    fix railtie

--- lib/active_validation.rb
+++ lib/active_validation.rb
@@ -1,16 +1,34 @@
 require 'active_model'
 require 'active_support'
-
-%w(version railtie)
-  .each do |file_name|
-    require "active_validation/#{file_name}"
-  end
+require 'active_validation/version'
 
 ACTIVE_VALIDATION_VALIDATORS ||= %w(
   alpha alpha_numeric base64 boolean coordinate credit_card currency cusip email equality hex imei
   ip isbn isin mac_address name password phone sedol slug ssn tracking_number type url username uuid
-)
+).freeze
 
 ACTIVE_VALIDATION_VALIDATORS.each do |file_name|
   require "active_validation/validators/#{file_name}_validator"
+end
+
+if defined?(Rails)
+  require 'rails'
+
+  module ActiveValidation
+    class Railtie < ::Rails::Railtie
+
+      initializer 'active_validation' do |app|
+        ActiveValidation::Railtie.instance_eval do
+          [app.config.i18n.available_locales].flatten.each do |locale|
+            (I18n.load_path << path(locale)) if File.file?(path(locale))
+          end
+        end
+      end
+
+      def self.path(locale)
+        File.expand_path("../../config/locales/#{locale}.yml", __FILE__)
+      end
+
+    end
+  end
 end
