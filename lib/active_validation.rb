c31e65cb5c0bef4cce0093b2d551514e08bf3c61
Juan Gomez <j.gomez@drexed.com>
2016-10-19 18:35:19 -0400

    railtie retry

--- lib/active_validation.rb
+++ lib/active_validation.rb
@@ -1,7 +1,7 @@
 require 'active_model'
 require 'active_support'
 
-%w(version railtie)
+%w(version)
   .each do |file_name|
     require "active_validation/#{file_name}"
   end
@@ -13,4 +13,28 @@ ACTIVE_VALIDATION_VALIDATORS ||= %w(
 
 ACTIVE_VALIDATION_VALIDATORS.each do |file_name|
   require "active_validation/validators/#{file_name}_validator"
+end
+
+if defined?(Rails)
+  require 'rails'
+
+  module ActiveValidation
+    class Railtie < ::Rails::Railtie
+
+      initializer 'active_validation' do |app|
+        ActiveValidation::Railtie.instance_eval do
+          [app.config.i18n.available_locales].flatten.each do |locale|
+            (I18n.load_path << path(locale)) if File.file?(path(locale))
+          end
+        end
+      end
+
+      protected
+
+      def self.path(locale)
+        File.expand_path("../../config/locales/#{locale}.yml", __FILE__)
+      end
+
+    end
+  end
 end
