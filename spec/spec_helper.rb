7b3a98c8da75dc41a9ea44a7ade0983b44f2c124
Juan Gomez <j.gomez@drexed.com>
2018-12-11 11:24:14 -0500

    clean up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,3 +16,8 @@ I18n.enforce_available_locales = false
 ACTIVE_VALIDATION_VALIDATORS.each do |file_name|
   require(spec_matchers_path.join("ensure_valid_#{file_name}_format_of.rb"))
 end
+
+RSpec.configure do |config|
+  config.include Shoulda::Matchers::ActiveModel
+  config.include Shoulda::Matchers::ActiveRecord
+end
